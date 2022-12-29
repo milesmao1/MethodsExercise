a60f13f9233ad9232af743723f2749172da43982
Miles Mao <mrmao1@gmail.com>
2022-12-29 12:20:18 -0600

    methods exercise results

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+             String userName = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            String userFavoriteColor = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+            String userFavoriteAnimal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite band?");
+            String userFavoriteBand = Console.ReadLine();
+            
+            Console.WriteLine()
         }
+        //Exercise 2
+        public static double Add(double x, double y) 
+        { 
+            return x + y;
+        }
+
+        public static int Multiply (int x, int y)
+        {
+          return x * y;
+
+        }
+
+        public static int Quotient (int x, int y)
+
+        { return x / y; }
+
+        public static int Subtract (int x, int y)
+        {
+            return x- y; }
+
+
+
+
     }
+
+
+
+
+
 }
